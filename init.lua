02321e721018914f5b7c51f886daf7b975f3617e
Josef van Niekerk <josef.van.niekerk@gmail.com>
2022-01-28 17:27:27 +0100

    Fix .lua files not loading correctly

--- init.lua
+++ init.lua
@@ -1,14 +1,15 @@
 -- Requires all files from the given folder within ./lua/{path} 
 --
 function requirePath(path) 
-  local count = 0
-  local files = io.popen('find ./lua/' .. path .. ' -type f')
+  local files = io.popen('find "$HOME"/.config/nvim/lua/' .. path .. ' -type f')
 
   for file in files:lines() do
-    local req_file = file:gmatch("(" .. path .. "%/.+)%.lua$"){0}:gsub("/", ".")
-    require (req_file)
-    vim.notify('Loaded ' .. req_file)
-    count = count + 1
+    local req_file = file:gmatch('%/lua%/(.+).lua$'){0}:gsub('/', '.')
+    status_ok, _ = pcall(require, req_file)
+
+    if not status_ok then
+      vim.notify('Failed loading ' .. req_file, vim.log.levels.ERROR)
+    end
   end
 end
 
